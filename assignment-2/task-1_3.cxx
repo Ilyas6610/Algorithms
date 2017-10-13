900b27da57e81182ac47f7da10c4f9e9fad0b651
Ilyas6610 <31966825+Ilyas6610@users.noreply.github.com>
2017-10-13 21:00:19 +0300

    Update task-1_3.cxx

--- assignment-2/task-1_3.cxx
+++ assignment-2/task-1_3.cxx
@@ -23,8 +23,62 @@ in  | out
 
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+    int n, minx = 1313144, miny = 2421411, minnum = 2412421;
+    cin >> n;
+    int *x = new int[n+1];
+    int *y = new int[n+1];
+    double *angle = new double[n+1];
+    for(int i=0;i<n;i++)
+    {
+        cin >> x[i] >> y[i];
+    };
+    for(int i=0;i<n;i++)
+    {
+        if(minx>x[i])
+        {
+            minx = x[i];
+            miny = y[i];
+            minnum = i;
+        }
+        else if(minx==x[i]&&miny>y[i])
+        {
+            miny = y[i];
+            minnum = i;
+        }
+    };
+    for(int i=0;i<n;i++)
+    {
+        if(minnum==i) continue;
+        angle[i]=((double)(y[i]-y[minnum])/(double)(x[i]-x[minnum]));
+    };
+    for(int i=0;i<n-1;i++)
+    {
+        if(i==minnum) continue;
+        for(int j=i+1;j<n;j++)
+        {
+            if(j==minnum) continue;
+            if(angle[i]<angle[j])
+            {
+                swap(x[i],x[j]);
+                swap(y[i],y[j]);
+                swap(angle[i],angle[j]);
+            }
+        }
+    };
+
+    cout << x[minnum] << ' ' << y[minnum] << endl;
+    for(int i=0;i<n;i++)
+    {
+        if(i==minnum) continue;
+        cout << x[i] << ' ' << y[i] << endl;
+    }
+    delete []x;
+    delete []y;
+    delete []angle;
     return 0;
 }
 
