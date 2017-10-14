2e3472830d484443767163a037db73c4590a1add
Ilyas6610 <31966825+Ilyas6610@users.noreply.github.com>
2017-10-15 00:22:57 +0300

    Update task-4_1.cxx

--- assignment-2/task-4_1.cxx
+++ assignment-2/task-4_1.cxx
@@ -11,9 +11,77 @@ in                 | out
 ****************************************************************************/
 
 #include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+void Merge( const int* a, int aLen, const int* b, int bLen, int* c ) {
+    int i = 0, j = 0;
+    while( i < aLen && j < bLen ) {
+        if( a[i] <= b[j] ) {
+            c[i + j] = a[i];
+            ++i;
+        } else {
+            c[i + j] = b[j];
+            ++j;
+        }
+    }
+    if( i == aLen ) {
+        for( ; j < bLen; ++j )
+            c[i + j] = b[j];
+        } else {
+        for( ; i < aLen; ++i )
+            c[i + j] = a[i];
+        }
+};
+
+void MergeSort( int* a, int aLen ) {
+    if( aLen <= 1 ) {
+        return;
+    }
+    int firstLen = aLen / 2;
+    int secondLen = aLen - firstLen;
+    MergeSort( a, firstLen );
+    MergeSort( a + firstLen, secondLen );
+    int* c = new int[aLen];
+    Merge( a, firstLen, a + firstLen, secondLen, c );
+    memcpy( a, c, sizeof( int ) * aLen );
+    delete[] c;
+}
+
+int maxK( int* a, int k )
+{
+    int d = a[0];
+    int maxn;
+    for( int i = 1; i < k ; i++ )
+        if( d < a[i] )
+        {
+            d = a[i];
+            maxn = i;
+        }
+    return maxn;
+}
 
 int main()
 {
+    int n, k = 0, m;
+    cin >> n >> k;
+    int* arr = new int[k];
+    for( int i = 0; i < k; i++ )
+        cin >> arr[i];
+    int e = maxK( arr, k );
+    for( int i = k; i < n; i++ )
+    {
+        cin >> m;
+        if( arr[e] > m )
+        {
+            arr[e] = m;
+            e = maxK( arr, k );
+        }
+    }
+    MergeSort( arr, k );
+    for( int i = 0; i < k; i++ )
+        cout << arr[i] << ' ';
     return 0;
 }
-
