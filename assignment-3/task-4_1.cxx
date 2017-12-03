1450df667c441515a00640a6935adf0572631d58
Ilyas6610 <31966825+Ilyas6610@users.noreply.github.com>
2017-12-03 18:38:41 +0300

    Update task-4_1.cxx
    Падает с рантаймом на 2, но я пробовал кодировать войну и мир (полностью), нормально кодировал (писал ОК).

--- assignment-3/task-4_1.cxx
+++ assignment-3/task-4_1.cxx
@@ -50,10 +50,156 @@
 
 ****************************************************************************/
 
+#include "Huffman.h"
 #include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <list>
 
-int main()
-{
-    return 0;
+using namespace std;
+
+struct Node{
+    Node(): Left( nullptr ), Right( nullptr ), isLeaf( false ) {};
+    Node( char a, int b, bool c ):
+        Key( a ),
+        Amount( b ),
+        isLeaf( c ),
+        Right( nullptr ),
+        Left( nullptr )
+    {};
+    Node( Node* a, Node* b ):
+        Left( a ),
+        Right( b ),
+        isLeaf( false )
+    {
+        Amount = a->Amount + b->Amount;
+    };
+    ~Node(){
+        delete Left;
+        delete Right;
+    }
+    char Key;
+    int Amount;
+    bool isLeaf;
+    Node* Left;
+    Node* Right;
+};
+
+struct Comp{
+    bool operator()( Node* a, Node* b ){
+        return a->Amount < b->Amount;
+    }
+};
+
+map<char, vector<char> > Code;
+vector<char> Codes;
+Node* root;
+
+void CodeGen( Node* node ){
+
+    if ( node->Left != nullptr ) {
+        Codes.push_back('0');
+        CodeGen( node->Left );
+    }
+
+    if ( node->Right != nullptr ){
+        Codes.push_back('1');
+        CodeGen( node->Right );
+    }
+
+    if ( node->isLeaf )
+        Code.emplace( node->Key, Codes );
+
+    Codes.pop_back();
 }
 
+int BitsAmount( int& l ){
+    if( l >= 8 )
+    {
+        l -= 8;
+        return 8;
+    }
+    else return l;
+}
+
+int Full = 0;
+
+void Encode(IInputStream& original, IOutputStream& compressed) {
+    byte value;
+    vector<char> Buff;
+    map<char, int> a;
+    map<char, int>::iterator i;
+    while(original.Read(value)) {
+        Buff.push_back(value);
+        a[value]++;
+    }
+    list<Node*> K;
+    for( i = a.begin(); i != a.end(); i++ ){
+        Node* node = new Node( i->first, i->second, true );
+        K.push_back( node );
+    };
+    while( K.size() != 1 ){
+        K.sort(Comp());
+
+        Node* left = K.front();
+        K.pop_front();
+
+        Node* right = K.front();
+        K.pop_front();
+
+        Node* Parent = new Node( left, right );
+
+        K.push_front( Parent );
+    };
+    root = K.front();
+    K.pop_front();
+    CodeGen( root );
+    int n = 0;
+    int l = 0;
+    char s[8] = "";
+
+    while( n != Buff.size() ){
+        for( int j = 0; j < Code[Buff[n]].size(); j++ )
+            Full++;
+        n++;
+    }
+    n = 0;
+    while( n != Buff.size() ){
+        for( int j = 0; j < Code[Buff[n]].size(); j++ ){
+            s[l] = Code[Buff[n]][j];
+            l++;
+            if( l == 8 || ( n == Buff.size() - 1 && j == Code[Buff[n]].size() - 1 ) ){
+                char* c;
+                int k = strtol(s, &c, 2);
+                char m = (char)k;
+                compressed.Write(m);
+                for( int i = 0; i < 8; i++ )
+                    s[i] = '\0';
+                l = 0;
+            }
+        }
+        n++;
+    }
+}
+
+void Decode(IInputStream& compressed, IOutputStream& original) {
+	Node *ptr = root;
+	byte cur;
+	int bits;
+    while(compressed.Read(cur)) {
+        bits = BitsAmount( Full );
+        for (int i = bits - 1; i >= 0; i--){
+            int bit = (cur >> i) & 1;
+            if ( !bit )
+                ptr = ptr->Left;
+            else
+                ptr = ptr->Right;
+
+            if ( ptr->isLeaf ) {
+                original.Write(ptr->Key);
+                ptr = root;
+            }
+        }
+    }
+}
