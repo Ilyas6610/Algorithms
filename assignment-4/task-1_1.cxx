d7cbf884d778bcd8cf30d74c9edf79ad4d567355
Ilyas6610 <31966825+Ilyas6610@users.noreply.github.com>
2017-12-11 21:12:31 +0300

    Update task-1_1.cxx

--- assignment-4/task-1_1.cxx
+++ assignment-4/task-1_1.cxx
@@ -54,10 +54,124 @@ FAIL, в зависимости от того, встречается ли да�
 
 ****************************************************************************/
 
+#include <vector>
+#include <string>
 #include <iostream>
 
-int main()
-{
-    return 0;
+using namespace std;
+
+const int InitialHashTableSize = 30;
+const int HashParameter = 28;
+
+int Hash( const string &data, int m, int probe ) {
+    int hash = 0;
+    for ( int i = 0; i < data.length(); ++i )
+        hash = ( hash * HashParameter + data[i] ) % m;
+    int c1 = 1;
+    int c2 = 1;
+    return ( hash + c1 * probe + c2 * probe * probe ) % m;
 }
 
+struct CHashTableNode {
+    string Data;
+    bool isDeleted;
+    CHashTableNode( const string &data ) :
+        Data( data ),
+        isDeleted( false )
+    {};
+};
+
+class CHashTable {
+public:
+    CHashTable() {
+        table.assign( InitialHashTableSize, NULL );
+        count = 0;
+    }
+    bool Has( const string &key ) const {
+        for ( int probe = 0; probe < table.size(); ++probe ) {
+            int hash = Hash( key, table.size(), probe );
+            if ( table[hash] != NULL && table[hash]->Data == key && !table[hash]->isDeleted )
+                return true;
+            else if ( table[hash] == NULL )
+                break;
+        }
+        return false;
+    }
+
+    bool Add( const string &key ) {
+        int hash;
+        int insert_place = -1;
+        for ( int probe = 0; probe < table.size(); ++probe ) {
+            hash = Hash( key, table.size(), probe );
+            if ( table[hash] != NULL && table[hash]->Data == key && !table[hash]->isDeleted )
+            	return false;
+            else if ( table[hash] == NULL ) {
+				if ( insert_place == -1 )
+                    insert_place = hash;
+				break;
+			} else if ( table[hash]->isDeleted )
+				if (insert_place == -1)
+                    insert_place = hash;
+        }
+        if ( insert_place == -1 )
+        	return false;
+        if ( table[insert_place] != NULL )
+        	delete table[insert_place];
+        table[insert_place] = new CHashTableNode( key );
+        ++count;
+        if ( count >= table.size() * 2 / 3 )
+            Rehash();
+        return true;
+    }
+
+    bool Delete( const string &key ) {
+        int hash;
+        for ( int probe = 0; probe < table.size(); ++probe ) {
+            hash = Hash( key, table.size(), probe );
+            if ( table[hash] != NULL && table[hash]->Data == key && !table[hash]->isDeleted )
+                break;
+            else if ( table[hash] == NULL )
+                return false;
+        }
+        if ( table[hash] == NULL )
+            return false;
+        table[hash]->isDeleted = true;
+        --count;
+        return true;
+    }
+
+private:
+    vector<CHashTableNode*> table;
+    int count;
+    void Rehash() {
+        vector<CHashTableNode*> old_table = table;
+        table.assign(table.size() * 2, NULL);
+        count = 0;
+        for ( int i = 0; i < old_table.size(); ++i ) {
+            if ( old_table[i] != NULL && !old_table[i]->isDeleted ) {
+                Add( old_table[i]->Data );
+                delete old_table[i];
+            }
+        }
+    }
+};
+
+int main() {
+    CHashTable strings;
+    while (true) {
+        if ( cin.eof() )
+            break;
+        char k;
+        string str;
+        cin >> k >> str;
+        if ( str.empty() )
+            break;
+        if ( k == '+' )
+            cout << ( strings.Add( str ) ? "OK" : "FAIL" ) << endl;
+        else if ( k == '-' )
+            cout << ( strings.Delete( str ) ? "OK" : "FAIL" ) << endl;
+        else if ( k == '?' )
+            cout << ( strings.Has( str ) ? "OK" : "FAIL" ) << endl;
+    }
+    return 0;
+}
