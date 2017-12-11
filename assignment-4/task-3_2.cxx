41099333efb7ccfa34124e9a635cae8e8ee2a5bf
Ilyas6610 <31966825+Ilyas6610@users.noreply.github.com>
2017-12-11 21:15:37 +0300

    Update task-3_2.cxx

--- assignment-4/task-3_2.cxx
+++ assignment-4/task-3_2.cxx
@@ -30,7 +30,37 @@ N ≤ 300.
 
 #include <iostream>
 
-int main()
-{
-    return 0;
+using namespace std;
+
+void PirWidth( long long** arr, int a ){
+    for ( int i = 0; i < a * 2 + 1; i++ ) {
+        for ( int k = 1; k <= i + 1; k++ ) {
+            int n = i - k + 1;
+            if ( n == 0 )
+                arr[n][k]=1;
+            else
+            {
+                arr[n][k] = 0;
+                for ( int j = 1; j <= n; j++ ){
+                    int i = n - j;
+                    if ( j < k )
+                        arr[n][k] += arr[i][j];
+                }
+            }
+        }
+    }
+}
+
+int main() {
+	int a;
+	cin >> a;
+	long long **arr = new long long *[a * 2 + 1];
+	for ( int i = 0; i < a * 2 + 1; i++ )
+		arr[i] = new long long[a * 2 + 1];
+    PirWidth( arr, a );
+	cout << arr[a][a] + 1;
+	for( int i = 0; i < a * 2 + 1; i++ )
+        delete [] arr[i];
+	delete []arr;
+	return 0;
 }
