bc36f6d817f32600f5963652e46be2f69b60f1fa
Ilyas6610 <31966825+Ilyas6610@users.noreply.github.com>
2017-12-11 21:13:39 +0300

    Update task-2_2.cxx

--- assignment-4/task-2_2.cxx
+++ assignment-4/task-2_2.cxx
@@ -36,9 +36,83 @@
 
 
 #include <iostream>
+#include <stdio.h>
+#include <limits.h>
+
+using namespace std;
+
+void siftDown( int *a, int *b, int k, int N);
+
+void swap(int *m, int *n)
+{
+	int tmp;
+	tmp = *m;
+	*m = *n;
+	*n = tmp;
+}
+
+void heapsort( int *a, int *b, int N){
+	for ( int k = N/2; k >= 0; k-- )
+		siftDown( a, b, k, N );
+
+	while ( N-1 > 0 ) {
+		swap( a[N-1], a[0] );
+		swap( b[N-1], b[0] );
+		siftDown( a, b, 0, N-1 );
+		N--;
+	}
+}
+
+void siftDown( int *a, int *b, int k, int N ){
+	while ( k*2 + 1 < N ) {
+ 		int child = 2*k + 1;
+		if ( ( child + 1 < N ) && ( a[child] < a[child+1] ) )
+            child++;
+		if ( a[k] < a[child] ) {
+			swap( a[child], a[k] );
+			swap( b[child], b[k] );
+			k = child;
+		}
+		else
+			return;
+	}
+}
+
 
 int main()
 {
+    int n = 0, a[100000], b[100000], mn = -1, m = INT_MAX;
+    while( !cin.eof() ){
+        cin >> a[n] >> b[n];
+        n++;
+    };
+    if( a[n] == 0 && b[n] == 0 )
+        n--;
+    heapsort( a, b, n );
+    for( int i = 0; i < n; i++ )
+        if( m > b[i] ){
+            m = b[i];
+            mn = i;
+        };
+    int e = 1, sum = 0;
+    if( mn != -1 )
+        sum++;
+    for( int j = 0; j < n; j++ ){
+        int k = INT_MAX;
+        e = 0;
+        for( int i = 0; i < n; i++ ){
+            if( m <= a[i] && k > b[i] ){
+                k = b[i];
+                mn = i;
+                e++;
+            }
+        }
+        m = b[mn];
+        if( e > 0 )
+            sum++;
+        if( e == 0 )
+            break;
+    };
+    cout << sum;
     return 0;
 }
-
