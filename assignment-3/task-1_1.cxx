1bc0193dd051d2eb2f2683cba2da302e9368ae7e
Ilyas6610 <31966825+Ilyas6610@users.noreply.github.com>
2019-10-17 22:05:22 +0300

    Update task-1_1.cxx

--- assignment-3/task-1_1.cxx
+++ assignment-3/task-1_1.cxx
@@ -28,9 +28,102 @@ root→Key ≤ K, то узел K добавляется в правое под�
 ****************************************************************************/
 
 #include <iostream>
+#include <stdio.h>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+struct Node
+{
+   int Key;
+   Node *left,*right;
+   bool isLeftChildVisited = 0;
+};
+
+
+void del(Node *&Tree){
+   if (Tree!=NULL)
+	{
+	   del(Tree->left);
+	   del(Tree->right);
+	   delete Tree;
+	   Tree = NULL;
+	}
+
+}
+
+void add(int Key,Node *&MyTree)
+{
+	if (NULL==MyTree)
+    {
+		MyTree=new Node;
+		MyTree->Key=Key;
+		MyTree->left=MyTree->right=NULL;
+	}
+    if (Key<MyTree->Key)
+    {
+        if (MyTree->left!=NULL) add(Key,MyTree->left);
+        else
+        {
+            MyTree->left=new Node;
+            MyTree->left->left=MyTree->left->right=NULL;
+            MyTree->left->Key=Key;
+        }
+    }
+    if (Key>MyTree->Key)
+    {
+        if (MyTree->right!=NULL) add(Key,MyTree->right);
+        else
+        {
+        MyTree->right=new Node;
+        MyTree->right->left=MyTree->right->right=NULL;
+        MyTree->right->Key=Key;
+        }
+    }
+}
+
+void leftNodes( Node* root, stack<Node*>& roots ){
+    while( root->left ){
+        roots.push( root->left );
+        root = root->left;
+    }
+}
+
+void inOrder(Node* root) {
+    stack<Node*> roots;
+    roots.push(root);
+    while( roots.size() > 0 ){
+        Node* node = roots.top();
+        if( node->left && node->isLeftChildVisited == 0 ){
+            roots.push( node->left );
+            node->isLeftChildVisited = 1;
+        }
+        else{
+            if( node->right ){
+                cout << node->Key << ' ';
+                roots.pop();
+                roots.push( node->right );
+            }else{
+                cout << node->Key << ' ';
+                roots.pop();
+            }
+        }
+    }
+    cout << endl;
+}
 
 int main()
 {
+    int n,x;
+    cin >> n;
+    Node* Tree = NULL;
+    for(int i=0;i<n;i++)
+    {
+        cin >> x;
+        add(x,Tree);
+    };
+    inOrder(Tree);
+    del(Tree);
     return 0;
 }
-
